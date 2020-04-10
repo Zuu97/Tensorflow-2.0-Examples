b127d48b33a297349f43d3efe0a65e750e4ad6a5
Isuru Alagiyawanna <isurualagiyawanna9717@gmail.com>
2020-04-10 13:01:54 +0530

    # mnist ANN

---
+++ Artifitial Neural Networks/util.py
@@ -0,0 +1,15 @@
+from variables import *
+import numpy as np
+import pandas as pd
+from sklearn.utils import shuffle
+def read_csv_files(csv_file):
+        data = pd.read_csv(csv_file).values.astype(np.float32)
+        data = shuffle(data)
+        Y = data[:,0]
+        X = data[:,1:] / 255.0
+        return X, Y
+
+def get_data():
+    Xtrain, Ytrain = read_csv_files(train_path)
+    Xtest , Ytest  = read_csv_files(test_path)
+    return Xtrain, Ytrain, Xtest , Ytest
---
+++ Artifitial Neural Networks/variables.py
@@ -0,0 +1,11 @@
+num_epochs = 15
+batch_size = 100
+tensor_shape = 784
+dense1 = 64
+dense2 = 32
+output = 10
+
+train_path = 'mnist_train.csv'
+test_path = 'mnist_test.csv'
+saved_model = 'mnist_classifier.json'
+saved_weights = 'mnist_classifier.h5'
