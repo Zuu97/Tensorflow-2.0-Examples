d8ec61dcabeb182d0018de31c600c7875179431c
Isuru Alagiyawanna <isurualagiyawanna9717@gmail.com>
2020-04-10 15:31:48 +0530

    # CIFAR10 classifier

--- Convolutional Neural Networks/util.py
+++ Convolutional Neural Networks/util.py
@@ -1,4 +1,5 @@
 from variables import *
+import pickle
 import numpy as np
 import pandas as pd
 from sklearn.utils import shuffle
@@ -11,6 +12,38 @@ def read_csv_files(csv_file):
         return X, Y
 
 def getMnistdata():
-    Xtrain, Ytrain = read_csv_files(train_path)
-    Xtest , Ytest  = read_csv_files(test_path)
-    return Xtrain, Ytrain, Xtest , Ytest
+        Xtrain, Ytrain = read_csv_files(train_path)
+        Xtest , Ytest  = read_csv_files(test_path)
+        return Xtrain, Ytrain, Xtest , Ytest
+
+def extract_batch_data(file):
+        with open(file, 'rb') as data:
+                data_dict = pickle.load(data, encoding='bytes')
+        return data_dict
+
+def get_train_data():
+        train_labels = []
+        train_images = []
+        for i in range(1, n_batches + 1):
+                batch_file = train_batch_prefix + str(i)
+                data_dict = extract_batch_data(batch_file)
+                train_labels.extend(data_dict[b'labels'])
+                train_images.extend(data_dict[b'data'])
+        all_train_labels = np.array(train_labels)
+        all_train_images = np.array(train_images).reshape(-1,*tensor_shape)
+        all_train_labels, all_train_images = shuffle(all_train_labels, all_train_images)
+        return all_train_labels, all_train_images
+
+
+def get_test_data():
+        data_dict = extract_batch_data(test_batch)
+        test_images = data_dict[b'data'].reshape(-1,*tensor_shape)
+        test_labels = data_dict[b'labels']
+        all_test_labels, all_test_images = np.array(test_labels), np.array(test_images)
+        all_test_labels, all_test_images = shuffle(all_test_labels, all_test_images)
+        return all_test_labels, all_test_images
+
+def getCIFAR10data():
+        Ytrain, Xtrain = get_train_data()
+        Ytest, Xtest = get_test_data()
+        return Xtrain, Ytrain, Xtest , Ytest
--- Convolutional Neural Networks/variables.py
+++ Convolutional Neural Networks/variables.py
@@ -1,16 +1,33 @@
+import os
+data_dir = 'E:\My Projects\Tensorflow 2.0  Basics\Data'
 num_epochs = 15
 batch_size = 100
-tensor_shape = (28, 28,1)
 kernal_size = (3,3)
 pool_size = (2,2)
 stride = (2,2)
-conv1 = 32
-conv2 = 64
-conv3 = 128
+
+# # Get Mnist parameters
+# conv1 = 32
+# conv2 = 64
+# conv3 = 128
+# dense = 512
+# output = 10
+# tensor_shape = (28, 28, 1)
+# train_path = os.path.join(data_dir,'mnist_train.csv')
+# test_path = os.path.join(data_dir,'mnist_test.csv')
+# saved_model = os.path.join(data_dir,'mnist_cnn.json')
+# saved_weights =  os.path.join(data_dir,'mnist_cnn.h5')
+
+# Get Cifar10 parameters
+conv1 = 64
+conv2 = 128
+conv3 = 256
 dense = 512
 output = 10
-train_path = 'mnist_train.csv'
-test_path = 'mnist_test.csv'
-saved_model = 'mnist_cnn.json'
-saved_weights = 'mnist_cnn.h5'
-data_dir = 'E:\My Projects\Tensorflow 2.0  Basics\Data'
+keep_prob = 0.2
+tensor_shape = (32, 32, 3)
+train_batch_prefix = os.path.join(data_dir,'data_batch_')
+test_batch = os.path.join(data_dir,'test_batch')
+saved_model = os.path.join(data_dir,'cifar10.json')
+saved_weights =  os.path.join(data_dir,'cifar10.h5')
+n_batches = 5
